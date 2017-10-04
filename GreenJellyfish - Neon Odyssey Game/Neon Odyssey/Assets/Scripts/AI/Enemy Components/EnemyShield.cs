df0fc3317d5332c0f00bfc764a0c4922b04787aa
edohtron <edohtron@users.noreply.github.com>
2017-10-04 12:29:11 +1100

    housekeeping; moved/removed test prefabs and scripts

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/AI/Enemy Components/EnemyShield.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShield : MonoBehaviour {
+
+    //use if list size will vary
+    //public List<GameObject> shieldList = new List<GameObject>();
+
+    //use if list size is static
+    public GameObject[] shieldList = new GameObject[4];
+
+    //shields spawn location
+    public Transform shieldPos;
+
+    //shield health
+    public int shieldCharge;
+
+    // Use this for initialization
+    void Start () {
+
+        Instantiate(shieldList[Random.Range(0, shieldList.Length - 1)], 
+                    shieldPos.position, 
+                    shieldPos.rotation,
+                    transform);
+        
+        Debug.Log("enemyShield");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Keyboard Testing controls/CharController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharController : MonoBehaviour {
+
+    //BulletController bc;
+   
+    //PLAYER SPEED
+    public float speed = 10.0f;
+    
+
+   //public Sprite redSprite;
+   //Image img;
+
+    //GameObject bullet2;
+    //public Transform bulletSpawn1;
+    //public int bulletSpeed = 5;
+
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+        //bc = GetComponentInChildren<BulletController>();
+        //img = GameObject.FindGameObjectWithTag("Crosshair").GetComponent<Image>();
+       
+        //for private
+        //bullet2 = GameObject.FindGameObjectWithTag("Blue");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+    }
+
+
+}
