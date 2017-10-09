e5378892cdceee0eb60a0e236be2bac45fed45fb
samuelfenton <samuel.fenton@outlook.com>
2017-10-09 17:29:53 +1100

    Modified Manager to get correct player function

--- GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Managers/GameManager.cs
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Managers/GameManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using XboxCtrlrInput;
 
-public class GameManager : MonoBehaviour {
-
+public class GameManager : MonoBehaviour
+{
     public Canvas m_pauseCanvas;
 
     private Player1Health m_playerRef;
@@ -14,30 +14,31 @@ public class GameManager : MonoBehaviour {
     public List<Image> m_player1HealthImage;
     public List<Image> m_player2HealthImage;
 
-    private int m_player1Health= 4;
+    private int m_player1Health = 4;
     private int m_player2Health = 4;
 
     private bool m_menuVis = false;
-	// Use this for initialization
-	void Start ()
+
+    // Use this for initialization
+    void Awake()
     {
         GameObject[] players;
         players = GameObject.FindGameObjectsWithTag("Player");
 
         //Assign player 1
-        if (players[0].GetComponent<Player1Health>() != null)
-            m_playerRef = players[0].GetComponent<Player1Health>();
-        else if (players[1].GetComponent<Player1Health>() != null)
-            m_playerRef = players[1].GetComponent<Player1Health>();
+        if (players[0].GetComponentInChildren<Player1Health>() != null)
+            m_playerRef = players[0].GetComponentInChildren<Player1Health>();
+        else if (players[1].GetComponentInChildren<Player1Health>() != null)
+            m_playerRef = players[1].GetComponentInChildren<Player1Health>();
 
         m_pauseCanvas.gameObject.SetActive(m_menuVis);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         //Pausing Screen
-		if(Input.GetKeyDown(KeyCode.Escape) || XCI.GetButtonDown(XboxButton.Start))
+        if (Input.GetKeyDown(KeyCode.Escape) || XCI.GetButtonDown(XboxButton.Start))
         {
             m_menuVis = !m_menuVis;
             m_pauseCanvas.gameObject.SetActive(m_menuVis);
@@ -55,14 +56,14 @@ public class GameManager : MonoBehaviour {
             m_player1Health = m_playerRef.health;
 
             //Set all to be blacked out
-            foreach(Image healthBar in m_player1HealthImage)
+            foreach (Image healthBar in m_player1HealthImage)
             {
                 healthBar.GetComponent<CanvasRenderer>().SetAlpha(0.0f);
             }
 
 
             //Set correct number to be coloured
-            for(int i = 0; i < m_player1Health; i++)
+            for (int i = 0; i < m_player1Health; i++)
             {
                 m_player1HealthImage[i].GetComponent<CanvasRenderer>().SetAlpha(1.0f);
             }
