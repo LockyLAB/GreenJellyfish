24f119e7caa59ef0631f77a992b8197d3cab336d
samuelfenton <samuel.fenton@outlook.com>
2017-10-11 10:45:39 +1100

    Added splashscreen and menu manager

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void PlayGameBtn()
+    {
+        SceneManager.LoadScene("Greybox");
+    }
+    public void OptionsBtn()
+    {
+
+    }
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Managers/SplashScreenManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("ChangeToMainMenu", 4);
+    }
+
+    void ChangeToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
