75b09a6bf3e78368d9f744a11122584a7bb7547e
edohtron <edohtron@users.noreply.github.com>
2017-10-16 14:55:21 +1100

    added traps and enemy contact damage

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/AI/AI single scripts/ContactDamage.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactDamage : MonoBehaviour
+{
+    /* 
+    * PLACE THIS SCRIPT ON ENEMY OBJECT
+    * ENEMY OBJECT NEEDS: RIGIDBODY, TRIGGER COLLIDER    
+    */
+
+    //store colour of enemy
+    int enemyColour;
+
+	// Use this for initialization
+	void Start ()
+    {
+        enemyColour = this.gameObject.layer;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if collided objects' parent is Player and is not same colour as trap
+        if (other.transform.parent.gameObject.tag == "Player" && other.transform.parent.gameObject.layer != enemyColour)
+        {
+            //Check player to damage
+            if (other.gameObject.GetComponent<Player1Health>() != null)
+            {
+                other.gameObject.GetComponent<Player1Health>().takeDamage(1);
+            }
+
+            if (other.gameObject.GetComponent<Player2Health>() != null)
+            {
+                other.gameObject.GetComponent<Player2Health>().takeDamage(1);
+            }
+        }
+    }
+}
---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Traps/TrapDamage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapDamage : MonoBehaviour
+{
+    /* 
+    * PLACE THIS SCRIPT ON THE TRAP OBJECTS "COLLIDER" CHILD
+    * "COLLIDER" OB  
+    */
+
+    //store colour of trap
+    int trapColour;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //set current layer check to layer of current object
+        trapColour = this.gameObject.layer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //check if collided objects' parent is Player and is not same colour as trap
+        if (other.transform.parent.gameObject.tag == "Player" && other.transform.parent.gameObject.layer != trapColour)
+        {
+            //Check player to damage
+            if (other.gameObject.GetComponent<Player1Health>() != null)
+            {
+                 other.gameObject.GetComponent<Player1Health>().takeDamage(1);
+            }
+
+            if (other.gameObject.GetComponent<Player2Health>() != null)
+            {
+                 other.gameObject.GetComponent<Player2Health>().takeDamage(1);
+            }
+        }
+    }
+
+   
+
+}
