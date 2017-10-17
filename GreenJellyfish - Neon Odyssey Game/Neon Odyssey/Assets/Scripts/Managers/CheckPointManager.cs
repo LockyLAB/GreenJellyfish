7d1f5725641b493d9647b36ab191c3189af2f99b
edohtron <edohtron@users.noreply.github.com>
2017-10-17 16:46:43 +1100

    added checkpoint scripts

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Managers/CheckPointManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{
+    /*
+     * ATTACH SCRIPT TO 'CP Manager' GAME OBJECT OR EMPTY GAME OBJECT
+     */
+    public Vector3 currentCheckpoint = Vector3.zero;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    public void setCurrentCheckpoint(Vector3 pos)
+    {
+        currentCheckpoint = pos;
+    }
+}
