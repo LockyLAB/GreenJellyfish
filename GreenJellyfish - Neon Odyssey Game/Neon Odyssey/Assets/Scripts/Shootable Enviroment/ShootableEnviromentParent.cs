9e3db1b3c7c783a9de7470d52b8c388530222cbd
samuelfenton <samuel.fenton@outlook.com>
2017-10-23 14:09:24 +1100

    Created shootable environments

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Shootable Enviroment/ShootableEnviromentParent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootableEnviromentParent : MonoBehaviour
+{
+    private List<GameObject> m_children= new List<GameObject>();
+
+    public void AddChild(GameObject child)
+    {
+        m_children.Add(child);
+    }
+
+    public void RemoveChild(GameObject child)
+    {
+        int index = m_children.IndexOf(child);
+        m_children.RemoveAt(index);
+    }
+
+    private void Update()
+    {
+        if (m_children.Count == 0)
+            Destroy(this.gameObject);
+    }
+}
---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Shootable Enviroment/ShootableEnviromentTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootableEnviromentTrigger : MonoBehaviour
+{
+    public GameObject m_parent = null;
+
+    private void Awake()
+    {
+        m_parent.GetComponent<ShootableEnviromentParent>().AddChild(this.gameObject);
+    }
+
+    public void OnDestruction()
+    {
+        m_parent.GetComponent<ShootableEnviromentParent>().RemoveChild(this.gameObject);
+        Destroy(this.gameObject);
+    }
+}
