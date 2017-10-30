f08151e769f409aec2aaa55fe803927add021233
samuelfenton <samuel.fenton@outlook.com>
2017-10-31 10:50:00 +1100

    Added pickup functionality

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    protected virtual void ActivatePickup(GameObject other)
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<Player>() != null) // Picked up by player only
+        {
+            ActivatePickup(other.gameObject);
+        }
+    }
+}
