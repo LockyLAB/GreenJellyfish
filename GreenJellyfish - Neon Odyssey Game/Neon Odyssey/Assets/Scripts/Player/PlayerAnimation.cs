c7d28ed2388a24ca3a31c72a88e191b8f90cb39b
samuelfenton <samuel.fenton@outlook.com>
2017-10-31 14:28:21 +1100

    Added player animation script

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+
+    //Effects
+    //Setting up landing effect
+    private bool m_inAir = true;
+
+    public GameObject m_landingEffect = null;
+    public Vector3 m_landingEffectSpawnPos = Vector3.up * 0.1f;
+
+    Animator m_animator = null;
+    PlayerController m_playerController = null;
+
+    // Use this for initialization
+    void Start ()
+    {
+        m_animator = gameObject.GetComponent<Animator>();
+
+        m_playerController = GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Landing animation
+        if (m_inAir)
+        {
+            if (m_playerController.m_CollisionInfo.bottom) // Hit ground
+            {
+                Instantiate(m_landingEffect, transform.TransformPoint(m_landingEffectSpawnPos), Quaternion.identity);
+                m_inAir = false;
+            }
+        }
+        else
+        {
+            if (!m_playerController.m_CollisionInfo.bottom) //Just jumped
+            {
+                m_inAir = true;
+            }
+        }
+
+
+    }
+}
