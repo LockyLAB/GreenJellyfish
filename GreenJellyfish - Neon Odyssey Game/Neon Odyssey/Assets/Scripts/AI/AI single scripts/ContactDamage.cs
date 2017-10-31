367acf9f2f7b46b24faf82b8368ead754cb401ab
edohtron <edohtron@users.noreply.github.com>
2017-10-31 13:49:43 +1100

    fixed contactDamage

--- GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/AI/AI single scripts/ContactDamage.cs
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/AI/AI single scripts/ContactDamage.cs
@@ -4,30 +4,28 @@ using UnityEngine;
 
 public class ContactDamage : MonoBehaviour
 {
-    /* 
-    * PLACE THIS SCRIPT ON ENEMY OBJECT
-    * ENEMY OBJECT NEEDS: RIGIDBODY, TRIGGER COLLIDER    
-    */
+    //-----------------------------------------------------------------
+    //PLACE THIS SCRIPT ON OBJECT TO DEAL CONTACT DAMAGE
+    //ENEMY OBJECT NEEDS: TRIGGER COLLIDER, RIGIDBODY 
+    //-----------------------------------------------------------------
 
     //store colour of enemy
-    int enemyColour;
+    private int enemyColour;
 
 	// Use this for initialization
 	void Start ()
     {
-        enemyColour = this.gameObject.layer;
+        //set colour of this object to object parents layer
+        if (gameObject.GetComponent<ContactDamage>() == true)
+        {
+            enemyColour = this.gameObject.layer;
+        }
 	}
 	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
-
     private void OnTriggerEnter(Collider other)
     {
-        //check if collided objects' parent is Player and is not same colour as trap
-        if (other.transform.parent.gameObject.tag == "Player" && other.transform.parent.gameObject.layer != enemyColour)
+        //check if other object is Player and is different colour
+        if (other.gameObject.tag == "Player" && other.gameObject.layer != enemyColour)
         {
             //Check player to damage
             if (other.gameObject.GetComponent<PlayerHealth>() != null)
