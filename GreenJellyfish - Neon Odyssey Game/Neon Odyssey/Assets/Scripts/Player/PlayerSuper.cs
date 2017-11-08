00cede315f1f42dba81c72fae5b7b1fdca1f45d6
edohtron <edohtron@users.noreply.github.com>
2017-11-08 16:04:57 +1100

    added player-super scripts

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/PlayerSuper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSuper : MonoBehaviour {
+
+    private float startCharge = 0f;
+    public float currentCharge;
+
+    public Text powerChargeDisplay;
+    public Image powerBarFill;
+    public Image powerBarBackground;
+
+    private int p1;
+    private int p2;
+
+	// Use this for initialization
+	void Start () {
+
+        currentCharge = startCharge;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ( gameObject.GetComponent<Player>().isFirstPlayer == true )
+        {
+            powerBarFill.fillAmount = currentCharge / 100;
+            powerChargeDisplay.text = currentCharge.ToString("F0") + "%";
+        }   
+
+        if ( gameObject.GetComponent<Player>().isFirstPlayer == false )
+        {
+            powerBarFill.fillAmount = currentCharge / 100;
+            powerChargeDisplay.text = currentCharge.ToString("F0") + "%";
+        }
+	}
+
+    public void ChangeCharge(float num)
+    {
+        currentCharge += num;
+    }
+
+}
