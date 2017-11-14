a77a94ed14c445538074d44f9b8fe949d7510cec
edohtron <edohtron@users.noreply.github.com>
2017-11-14 14:12:59 +1100

    added boss/shield event trigger

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Shootable Enviroment/RayshieldTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayshieldTrigger : MonoBehaviour {
+
+    public GameObject boss;
+    public GameObject shield;
+    private bool triggered;
+
+    void Start()
+    {
+        //shield.SetActive(true);
+        triggered = false;
+    }
+    // Update is called once per frame
+    void Update () {
+        
+        if (!triggered)
+        {
+            if (boss == null)
+            {
+                shield.SetActive(false);
+                triggered = true;
+            }
+        }
+	}
+
+}
