c767ee8606bcf2abbf76e1ae06baf1441d668034
edohtron <edohtron@users.noreply.github.com>
2017-11-14 17:09:52 +1100

    added boss entry door script

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Shootable Enviroment/RayshieldEntry.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayshieldEntry : MonoBehaviour {
+
+    public GameObject shield;
+    
+    private GameObject playerOne;
+    private GameObject playerTwo;
+
+    private bool p1trigger = false;
+    private bool p2trigger = false;
+    private bool bothTriggeered = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerOne = GameObject.Find("Anubis");
+        playerTwo = GameObject.Find("Horus");
+        shield.SetActive(false);
+	}
+
+    void Update()
+    {
+        if (p1trigger == true && p2trigger == true)
+        {
+            bothTriggeered = true;
+            if (bothTriggeered == true)
+            {
+                shield.SetActive(true);
+                bothTriggeered = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == playerOne)
+        {
+            p1trigger = true;
+        }
+
+        if (other.gameObject == playerTwo)
+        {
+            p2trigger = true;
+        }
+    }
+
+
+
+}
