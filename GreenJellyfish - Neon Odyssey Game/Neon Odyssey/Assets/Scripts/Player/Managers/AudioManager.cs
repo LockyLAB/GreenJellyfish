67646dd615ba65627a2fb34896700b9b52e7624b
samuelfenton <samuel.fenton@outlook.com>
2017-11-20 10:23:22 +1100

    Changed audio manager to player initial loop and then looping track afterwards

--- GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/AudioManager.cs
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/AudioManager.cs
@@ -2,62 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudioManager : MonoBehaviour {
-
-    public float m_maxVolume = 1.0f;
-
-	// Use this for initialization
-	void Awake ()
-    {
-        if (m_maxVolume > 1.0f)
-            m_maxVolume = 1.0f;
-        if (m_maxVolume < 0.0f)
-            m_maxVolume = 0.0f;
-    }
+public class AudioManager : MonoBehaviour
+{
+    public GameObject m_musicIntro = null;
+    public GameObject m_musicLoop = null;
 
     // Update is called once per frame
     void Update()
     {
-        //Loop through all Audio Sources
-        foreach (AudioSource source in GetComponents<AudioSource>())
-        {
-            if (!source.isPlaying)
-                Destroy(source); //Remove Audio source game object when finished playing
-        }
+        if (!m_musicLoop.GetComponent<AudioSource>().isPlaying)
+            if(!m_musicIntro.GetComponent<AudioSource>().isPlaying)
+                m_musicLoop.GetComponent<AudioSource>().Play();
     }
-
-    ////Play sound
-    ////Example "Audio/Player/footstep"
-    //public GameObject GetSoundObject(string sound, Vector3 soundLocation)
-    //{
-    //    ////Set frequency to be random
-    //    //AudioClip soundClip = Resources.Load<AudioClip>(sound); //Set up sound
-    //    //AudioSource soundSource = gameObject.AddComponent<AudioSource>() as AudioSource;
-    //    //soundSource.volume = m_maxVolume;
-    //    //soundSource.clip = soundClip;
-
-    //    //GameObject newSound = new GameObject(); //Create object
-    //    //newSound.transform.position = soundLocation;
-
-    //    //return newSound;
-    //}
-
-    //public GameObject GetSoundObjectRndPitch(string sound, Vector3 soundLocation)
-    //{
-    //    ////Random value
-    //    //float rndPitch = Random.Range(0.0f, 1.0f);
-
-    //    ////Set frequency to be random
-    //    //AudioClip soundClip = Resources.Load<AudioClip>(sound);
-    //    //AudioSource soundSource = gameObject.AddComponent<AudioSource>();
-    //    //soundSource.volume = m_maxVolume;
-    //    //soundSource.pitch = rndPitch;
-    //    //soundSource.clip = soundClip;
-
-    //    //GameObject newSound = new GameObject(); //Create object
-    //    //newSound.transform.position = soundLocation;
-    //    //newSound.AddComponent<soundSource>();
-
-    //    //return newSound;
-    //}
 }
