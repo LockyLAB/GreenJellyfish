a45fb74f88e2a17216bfd352b78f6cc00e4626bc
samuelfenton <samuel.fenton@outlook.com>
2017-11-21 16:30:42 +1100

    New button script to change img when selected

---
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/ButtonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonScript : MonoBehaviour, ISelectHandler
+{
+    public Image m_selectedImg;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Color tempColor = m_selectedImg.color;
+        tempColor.a = 1.0f;
+        m_selectedImg.color = tempColor;
+    }
+
+    public void OnDeselect(BaseEventData data)
+    {
+        Color tempColor = m_selectedImg.color;
+        tempColor.a = 0.0f;
+        m_selectedImg.color = tempColor;
+    }
+}
--- GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/MenuManager.cs
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/MenuManager.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using XboxCtrlrInput;
 
 public class MenuManager : MonoBehaviour
 {
 
     public string m_firstLevel = "";
+
+    public Image m_splashImage = null;
+
+    public float m_splashFadeTime = 2.0f;
+    private float m_splashFadeTimer = 0.0f;
+
+    private bool m_splashFade = false;
+
+    //Main menu stuff
+    public Image m_PlayBtnImg;
+    public Image m_QuitBtnImg;
+
+    public Button m_PlayBtn;
+    public Button m_QuitBtn;
+
+    private void Update()
+    {
+        //Pausing Screen
+        if (XCI.GetButtonDown(XboxButton.Start))
+        {
+            m_splashFade = true;
+        }
+
+        if(m_splashFade && m_splashFadeTimer > m_splashFadeTime)
+        {
+            float alpha = m_splashFadeTimer / m_splashFadeTime;
+            Color tempColor = m_splashImage.color;
+            tempColor.a = 1- alpha;
+            m_splashImage.color = tempColor;
+        }
+    }
 
     //--------------------------------------------------------------------------------------
     // Load first level
