76932f94658184cf160c4f36a53ac1a1bdf9f3cc
samuelfenton <samuel.fenton@outlook.com>
2017-11-29 09:34:55 +1100

    Aded portal sound delay Music fades in at start of game

--- GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/AudioManager.cs
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/AudioManager.cs
@@ -12,15 +12,40 @@ public class AudioManager : MonoBehaviour
     public GameObject m_musicIntro = null;
     public GameObject m_musicLoop = null;
 
+    public float m_musicStartDelay = 1.0f;
+
+    public float m_musicFadeIn = 1.0f;
+    private float m_musicFadeInTimer = 0.0f;
+
+    private void Start()
+    {
+        Invoke("StartMusic", m_musicStartDelay);
+        m_musicIntro.GetComponent<AudioSource>().volume = 0;
+    }
+
     //--------------------------------------------------------------------------------------
     // Play initial track, at end, play looping track, destroy object
     //--------------------------------------------------------------------------------------
     void Update()
     {
+        while(m_musicFadeInTimer < m_musicFadeIn)
+        {
+            m_musicFadeInTimer += Time.deltaTime;
+            m_musicIntro.GetComponent<AudioSource>().volume = m_musicFadeInTimer/ m_musicFadeIn;
+        }
+
         if(!m_musicIntro.GetComponent<AudioSource>().isPlaying)
         {
             m_musicLoop.GetComponent<AudioSource>().Play();
-            Destroy(this);
+            //Destroy(this);
         }
+    }
+
+    //--------------------------------------------------------------------------------------
+    // Play initial track after delay
+    //--------------------------------------------------------------------------------------
+    void StartMusic()
+    {
+        m_musicIntro.GetComponent<AudioSource>().Play();
     }
 }
--- GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/IntroSequence.cs
+++ GreenJellyfish - Neon Odyssey Game/Neon Odyssey/Assets/Scripts/Player/Managers/IntroSequence.cs
@@ -18,6 +18,9 @@ public class IntroSequence : MonoBehaviour
     public GameObject m_portalWaterEffect = null;
     public GameObject m_portalActivationSound = null;
 
+    public float m_portalEffectDelay = 1.0f;
+    private bool m_portalEffectActivated = false;
+
     private bool m_cameraEnabled = false;
 
     public float m_portalWaterTime = 1.0f;
@@ -46,19 +49,30 @@ public class IntroSequence : MonoBehaviour
     private void Update()
     {
         m_sequenceTimer += Time.deltaTime;
+
+        if (m_sequenceTimer > m_portalEffectDelay && !m_portalEffectActivated) // Activate portal water
+        {
+            GameObject activationSound = Instantiate(m_portalActivationSound, Vector3.zero, Quaternion.identity);
+            activationSound.GetComponent<AudioSource>().Play();
+            Destroy(activationSound, 5.0f);
+
+            m_portalEffectActivated = true;
+        }
+
         if (m_sequenceTimer > m_portalWaterTime && !m_portalWaterActivated) // Activate portal water
         {
             m_portalWaterEffect.SetActive(true);
             m_portalWaterActivated = true;
         }
 
-        else if (m_sequenceTimer > m_animationTime && !m_cameraEnabled)//Enable camera and player input at end of timer
+        if (m_sequenceTimer > m_animationTime && !m_cameraEnabled)//Enable camera and player input at end of timer
         {
             m_cameraEnabled = true;
             GameObject.FindWithTag("MainCamera").GetComponent<CameraMove>().m_dynamicCamera = true;
             Destroy(m_cameraAnimator);
         }
-        else if(m_sequenceTimer > m_sequenceTime) //Enable player montion at end of opening animation
+
+        if(m_sequenceTimer > m_sequenceTime) //Enable player montion at end of opening animation
         {
             //Remove animators
             Destroy(m_player1Animator);
